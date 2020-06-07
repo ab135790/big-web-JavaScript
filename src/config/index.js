6f338aa4d2e4efd85f122e08692ad856745c5dc0
WJN06 <275284429@qq.com>
2020-06-07 22:03:15 +0800

    2020-6-7 last

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-05-17 16:07:29
- * @LastEditTime: 2020-06-07 17:34:20
+ * @LastEditTime: 2020-06-07 22:02:35
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \big-web-JavaScript\src\config\index.js
@@ -10,7 +10,8 @@ export default {
   baseUrl: {
     // dev: 'http://localhost:3000',
     dev: 'http://localhost:36742', // mockServer
-    pro: 'http://192.168.27.130:12005'
+    // pro: 'http://192.168.27.130:12005'
+    pro: 'http://106.55.147.49:12005'
   },
   publicPath: [/^\/public/, /^\/login/]
 }
